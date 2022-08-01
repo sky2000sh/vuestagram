691938317f617d0f0ef4733d6574968f819e0b21
sky2000sh <sky2000sh@gmail.com>
2022-08-01 21:54:56 +0900

    first commit on 1st august

---
+++ src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Post/>
+        <Post/>
+        <Post/>
+    </div>
+</template>
+
+<script>
+import Post from './Post'
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Container',
+    components: {
+        Post : Post
+    }
+}
+</script>
+
+<style>
+
+</style>
