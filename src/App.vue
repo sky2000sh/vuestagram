2df066ca45f42f6a652254542f4806387072451e
sky2000sh <sky2000sh@gmail.com>
2022-08-08 22:38:29 +0900

    3rd commit on 8th august

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
   </div>
 
   <Container :postData="postData"/>
+  <button @click="more">더보기</button>
 
   <div class="footer">
     <ul class="footer-button-plus">
@@ -23,6 +24,9 @@
 import Container from './components/Container'
 import postdata from './assets/postdata'
 
+// 2022년 8월 8일 yarn add axios 추가
+import axios from 'axios'
+
 export default {
   name: 'App',
   components: {
@@ -33,7 +37,45 @@ export default {
     return {
       postData : postdata
     }
-  }
+  },
+
+  methods: {
+
+    more() {
+      axios.get('https://codingapple1.github.io/vue/more0.json').then(
+        // function(param) {
+        //   param
+        //   console.log('이 data는? :', param.data)
+        // }
+        // 위 function 함수와 아래 애로우 함수는 같은 것이다.
+        // 애로우 함수의 장점은 소괄호에 들어가는 파라미터가 한 개뿐이라면 괄호 생략이 가능하다. 즉 그저 param =>
+        (param)=> {
+          param
+          console.log('이 data는? :', param.data)
+          this.postData.push(param.data)
+        }
+      )
+
+      // axios 개발 몇가지 더!
+      // post() 함수 => 내가 요청한 데이터를 전송하게 해준다.
+      let url = 'https://codingapple1.github.io/vue/more0.jso'
+      axios.post(url, {name : 'kim'})
+      .then(
+        console.log('여기는 성공!!!')
+      )
+      .catch(
+        // (err)=> {
+        //   err
+        //   console.log('이 err? :', err)
+        // }
+        console.log('여기는 실패!!!')
+      )
+      // 위의 요청이 성공시는 then()   /   실패시는 catch() 함수에 메시지를 담는다.
+    }
+    
+  },
+
+  
 }
 </script>
 
