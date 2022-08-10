b4a1762078032e4e81ec9dc9f01b667db066d737
sky2000sh <sky2000sh@gmail.com>
2022-08-10 22:13:16 +0900

    5th commit on 10th august

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     <img src="./assets/logo.png" class="logo" />
   </div>
 
-  <Container :postData="postData"/>
+  <Container :postData="postData" :step="step" />
   <button @click="more">더보기</button>
 
   <div class="footer">
@@ -18,6 +18,14 @@
       <label for="file" class="input-plus">+</label>
     </ul>
   </div>
+
+  <!-- <div v-if="step == 0">내용0</div>
+  <div v-if="step == 1">내용1</div>
+  <div v-if="step == 2">내용2</div>
+  <button @click="step = 0">버튼0</button>
+  <button @click="step = 1">버튼1</button>
+  <button @click="step = 2">버튼2</button> -->
+
 </template>
 
 <script>
@@ -35,7 +43,8 @@ export default {
 
   data() {
     return {
-      postData : postdata
+      postData : postdata,
+      step : 0,
     }
   },
 
